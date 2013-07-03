12781754519c63f4241d16948911d2cc5e21a3cb
Scott Warner <Tortel1210@gmail.com>
2013-07-03 12:22:56 -0400

    Use the support lib from ABS, and use it

--- src/com/tortel/deploytrack/MainActivity.java
+++ src/com/tortel/deploytrack/MainActivity.java
@@ -1,10 +1,11 @@
 package com.tortel.deploytrack;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
 
-public class MainActivity extends Activity {
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.Menu;
+
+public class MainActivity extends SherlockFragmentActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +16,7 @@ public class MainActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getSupportMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
