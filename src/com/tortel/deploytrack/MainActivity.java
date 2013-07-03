bb24c58fb0db515d947b8d32bd87f39fe632d29f
Scott Warner <Tortel1210@gmail.com>
2013-07-03 12:39:58 -0400

    Make menu consistent

--- src/com/tortel/deploytrack/MainActivity.java
+++ src/com/tortel/deploytrack/MainActivity.java
@@ -1,11 +1,13 @@
 package com.tortel.deploytrack;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.Menu;
 
 public class MainActivity extends SherlockFragmentActivity {
+	private Menu settingsMenu;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +19,18 @@ public class MainActivity extends SherlockFragmentActivity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getSupportMenuInflater().inflate(R.menu.main, menu);
+		settingsMenu = menu;
 		return true;
 	}
 
+	@Override
+	public boolean onKeyUp(int keyCode, KeyEvent e){
+		switch(keyCode){
+		case KeyEvent.KEYCODE_MENU:
+			settingsMenu.performIdentifierAction(R.id.full_menu_settings, 0);
+			return true; 
+		}
+		
+		return super.onKeyUp(keyCode, e);
+	}
 }
