60d8c7eb7a05ded9b220eca4db47119350566fbd
Scott Warner <Tortel1210@gmail.com>
2013-12-02 19:43:08 -0500

    Format dat code

--- src/com/tortel/deploytrack/service/NotificationService.java
+++ src/com/tortel/deploytrack/service/NotificationService.java
@@ -35,116 +35,127 @@ import android.widget.RemoteViews;
 import android.widget.Toast;
 
 public class NotificationService extends Service {
-	private static final String KEY_ID = "id";
-	private static final int NOTIFICATION_ID = 1234;
-	private static final boolean DEBUG = true;
-	private static final int SIZE = 250;
-	
-	private int deploymentId;
-	private NotificationManager notificationManager;
-	
-	
-	@Override
-	public void onCreate() {
-		super.onCreate();
-		notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-		deploymentId = getSavedId(this);
-		showNotification();
-		if(DEBUG) Toast.makeText(this, "NotificationService onCreate", Toast.LENGTH_SHORT).show();
-		//TODO: Schedule a refresh of the notification at midnight
-	}
+    private static final String KEY_ID = "id";
+    private static final int NOTIFICATION_ID = 1234;
+    private static final boolean DEBUG = true;
+    private static final int SIZE = 250;
 
-	@Override
-	public void onDestroy() {
-		super.onDestroy();
-		notificationManager.cancel(NOTIFICATION_ID);
-		
-		if(DEBUG) Toast.makeText(this, "NotificationService onDestroy", Toast.LENGTH_SHORT).show();
-	}
+    private int deploymentId;
+    private NotificationManager notificationManager;
 
-	@Override
-	public int onStartCommand(Intent intent, int flags, int startId) {
-		deploymentId = getSavedId(this);
-		//Reload the notification
-		showNotification();
-		//Stick damn you!
-		return START_STICKY;
-	}
-	
-	@SuppressLint("NewApi")
-	private void showNotification(){
-		//Cancel any existing notifications
-		notificationManager.cancel(NOTIFICATION_ID);
-		//If there isnt an ID saved, shut down the service
-		if(deploymentId == -1){
-			stopSelf();
-			return;
-		}
-		if(DEBUG) Toast.makeText(this, "NotificationService loading notification", Toast.LENGTH_SHORT).show();
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        deploymentId = getSavedId(this);
+        showNotification();
+        if (DEBUG)
+            Toast.makeText(this, "NotificationService onCreate", Toast.LENGTH_SHORT).show();
+        // TODO: Schedule a refresh of the notification at midnight
+    }
 
-		//Load the Deployment object
-		Deployment deployment = DatabaseManager.getInstance(this).getDeployment(deploymentId);
-		RemoteViews view = new RemoteViews(getPackageName(), R.layout.notification);
-		view.setImageViewBitmap(R.id.notification_pie, WidgetProvider.getChartBitmap(deployment, SIZE));
-		view.setTextViewText(R.id.notification_title, deployment.getName());
-		
-		view.setTextViewText(R.id.notification_main, getResources().getString(R.string.small_notification, deployment.getPercentage(),
-				deployment.getCompleted(), deployment.getLength()));
-		
-		view.setTextViewText(R.id.notification_daterange,getResources().getString(R.string.date_range, deployment.getFormattedStart(), deployment.getFormattedEnd()));
-		
-		
-		NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
-		builder.setContentTitle(deployment.getName());
-		builder.setContentText(
-				getResources().getString(R.string.small_notification, deployment.getPercentage(),
-						deployment.getCompleted(), deployment.getLength()));
-		builder.setOngoing(true);
-		//Hide the time, its persistent
-		builder.setWhen(0);
-		//TODO: Create a notification-suitable icon
-		builder.setSmallIcon(R.drawable.ic_launcher);
-		builder.setPriority(Integer.MAX_VALUE);
-		//TODO: Add a RemoteView to make it fancy and expandable
-		Notification notification = builder.build();
-		
-		if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN){
-			notification.bigContentView = view;
-		}
-		
-		notificationManager.notify(NOTIFICATION_ID, notification);
-	}
-	
-	/**
-	 * Load the ID for the deployment to be shown as a notification.
-	 * Returns -1 if one isn't saved yet
-	 */
-	public static int getSavedId(Context context) {
-		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
-		return prefs.getInt(KEY_ID, -1);
-	}
-	
-	/**
-	 * Sets the ID of the deployment to show as a notification
-	 * @param context
-	 * @param id the id to save
-	 */
-	public static void setSavedId(Context context, int id){
-		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
-		prefs.edit().putInt(KEY_ID, id).commit();
-	}
-	
-	@Override
-	public IBinder onBind(Intent intent) {
-		return new LocalBinder();
-	}
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        notificationManager.cancel(NOTIFICATION_ID);
 
-	/**
-	 * Simple binder to get access to the service
-	 */
-	public class LocalBinder extends Binder{
-		NotificationService getService(){
-			return NotificationService.this;
-		}
-	}
+        if (DEBUG)
+            Toast.makeText(this, "NotificationService onDestroy", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        deploymentId = getSavedId(this);
+        // Reload the notification
+        showNotification();
+        // Stick damn you!
+        return START_STICKY;
+    }
+
+    @SuppressLint("NewApi")
+    private void showNotification() {
+        // Cancel any existing notifications
+        notificationManager.cancel(NOTIFICATION_ID);
+        // If there isnt an ID saved, shut down the service
+        if (deploymentId == -1) {
+            stopSelf();
+            return;
+        }
+        if (DEBUG)
+            Toast.makeText(this, "NotificationService loading notification", Toast.LENGTH_SHORT)
+                    .show();
+
+        // Load the Deployment object
+        Deployment deployment = DatabaseManager.getInstance(this).getDeployment(deploymentId);
+        RemoteViews view = new RemoteViews(getPackageName(), R.layout.notification);
+        view.setImageViewBitmap(R.id.notification_pie,
+                WidgetProvider.getChartBitmap(deployment, SIZE));
+        view.setTextViewText(R.id.notification_title, deployment.getName());
+
+        view.setTextViewText(
+                R.id.notification_main,
+                getResources().getString(R.string.small_notification, deployment.getPercentage(),
+                        deployment.getCompleted(), deployment.getLength()));
+
+        view.setTextViewText(
+                R.id.notification_daterange,
+                getResources().getString(R.string.date_range, deployment.getFormattedStart(),
+                        deployment.getFormattedEnd()));
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        builder.setContentTitle(deployment.getName());
+        builder.setContentText(getResources().getString(R.string.small_notification,
+                deployment.getPercentage(), deployment.getCompleted(), deployment.getLength()));
+        builder.setOngoing(true);
+        
+        // Hide the time, its persistent
+        builder.setWhen(0);
+        
+        // TODO: Create a notification-suitable icon
+        builder.setSmallIcon(R.drawable.ic_launcher);
+        builder.setPriority(Integer.MAX_VALUE);
+        
+        Notification notification = builder.build();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            notification.bigContentView = view;
+        }
+
+        notificationManager.notify(NOTIFICATION_ID, notification);
+    }
+
+    /**
+     * Load the ID for the deployment to be shown as a notification. Returns -1
+     * if one isn't saved yet
+     */
+    public static int getSavedId(Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        return prefs.getInt(KEY_ID, -1);
+    }
+
+    /**
+     * Sets the ID of the deployment to show as a notification
+     * 
+     * @param context
+     * @param id
+     *            the id to save
+     */
+    public static void setSavedId(Context context, int id) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        prefs.edit().putInt(KEY_ID, id).commit();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return new LocalBinder();
+    }
+
+    /**
+     * Simple binder to get access to the service
+     */
+    public class LocalBinder extends Binder {
+        NotificationService getService() {
+            return NotificationService.this;
+        }
+    }
 }
