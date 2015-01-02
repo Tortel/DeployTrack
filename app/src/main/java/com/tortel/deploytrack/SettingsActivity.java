f50c8f2860d96ae6d27465d712792be84efdc18d
Scott Warner <Tortel1210@gmail.com>
2015-01-02 14:44:19 -0500

    Fix up theme and apply theme change right away

--- app/src/main/java/com/tortel/deploytrack/SettingsActivity.java
+++ app/src/main/java/com/tortel/deploytrack/SettingsActivity.java
@@ -17,13 +17,17 @@ package com.tortel.deploytrack;
 
 import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.support.v4.content.IntentCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.view.MenuItem;
 
 
 public class SettingsActivity extends ActionBarActivity {
+    private static final String KEY_THEME = "pref_light_theme";
 
 	@Override
 	public void onCreate(Bundle savedInstanceState){
@@ -67,12 +71,42 @@ public class SettingsActivity extends ActionBarActivity {
         sendBroadcast(updateWidgetIntent);
 	}
 
-    public static class SettingsFragment extends PreferenceFragment {
+    public static class SettingsFragment extends PreferenceFragment
+            implements SharedPreferences.OnSharedPreferenceChangeListener {
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
 
             addPreferencesFromResource(R.xml.preferences);
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            getPreferenceScreen().getSharedPreferences()
+                    .registerOnSharedPreferenceChangeListener(this);
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+            getPreferenceScreen().getSharedPreferences()
+                    .unregisterOnSharedPreferenceChangeListener(this);
+
+        }
+
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            // For theme changes, restart the app to apply it right away
+            if(key.equals(KEY_THEME)){
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                if(Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB){
+                    intent.setFlags(IntentCompat.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                } else {
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                }
+                getActivity().startActivity(intent);
+            }
         }
     }
 }
