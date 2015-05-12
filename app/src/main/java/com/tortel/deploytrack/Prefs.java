c006c4c285268da0fe24a68495c75e2dab017bab
Scott Warner <Tortel1210@gmail.com>
2015-05-12 09:49:05 -0400

    Make sure screenshot mode hides all dates/days

--- app/src/main/java/com/tortel/deploytrack/Prefs.java
+++ app/src/main/java/com/tortel/deploytrack/Prefs.java
@@ -102,6 +102,7 @@ public class Prefs {
     public static void setScreenShotMode(boolean screenShotMode, Context context){
         if(screenShotMode){
             hideDate = true;
+            hideDays = true;
             hidePercent = true;
         } else {
             load(context);
