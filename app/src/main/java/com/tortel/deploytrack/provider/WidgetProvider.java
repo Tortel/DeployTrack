72994d3b8eef863ba49b3e56cf2bc2f8a9457a83
Scott Warner <Tortel1210@gmail.com>
2020-06-02 13:35:51 -0400

    Clean up the WidgetProvider code

--- app/src/main/java/com/tortel/deploytrack/provider/WidgetProvider.java
+++ app/src/main/java/com/tortel/deploytrack/provider/WidgetProvider.java
@@ -27,7 +27,6 @@ import com.tortel.deploytrack.Prefs;
 import com.tortel.deploytrack.R;
 import com.tortel.deploytrack.data.*;
 
-import android.annotation.TargetApi;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
@@ -42,10 +41,11 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.RectF;
 import android.graphics.Region;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RemoteViews;
+
+import androidx.annotation.NonNull;
 
 /**
  * Class that manages updating the widgets
@@ -85,17 +85,20 @@ public class WidgetProvider extends AppWidgetProvider {
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager,
-            int[] appWidgetIds){
+            int[] appWidgetIds) {
         // Check if the database needs to be upgraded
-        if(DatabaseUpgrader.needsUpgrade(context)){
+        if (DatabaseUpgrader.needsUpgrade(context)) {
             DatabaseUpgrader.doDatabaseUpgrade(context);
         }
 
         updateAllWidgets(context, appWidgetManager);
 
         AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        if (alarmManager == null) {
+            return;
+        }
 
-        if(mScreenShotMode){
+        if (mScreenShotMode) {
             // Time screenshot mode out in 3 min
             Intent cancelScreenShotMode = new Intent(UPDATE_INTENT);
             cancelScreenShotMode.putExtra(KEY_SCREENSHOT_MODE, false);
@@ -104,12 +107,7 @@ public class WidgetProvider extends AppWidgetProvider {
                     cancelScreenShotMode, PendingIntent.FLAG_CANCEL_CURRENT);
 
             long triggerTime = new Date().getTime() + SCREENSHOT_TIMEOUT * MILIS_PER_MIN;
-
-            if(Build.VERSION.SDK_INT > Build.VERSION_CODES.JELLY_BEAN_MR2){
-                alarmManager.setExact(AlarmManager.RTC, triggerTime, screenshotPending);
-            } else {
-                alarmManager.set(AlarmManager.RTC, triggerTime, screenshotPending);
-            }
+            alarmManager.set(AlarmManager.RTC, triggerTime, screenshotPending);
         } else {
             //Schedule an update at midnight
             DateTime now = new DateTime();
@@ -120,29 +118,21 @@ public class WidgetProvider extends AppWidgetProvider {
 
             //Adding 100msec to make sure its triggered after midnight
             Log.d("Scheduling update for "+tomorrow.getMillis() + 100);
-
-            if(Build.VERSION.SDK_INT > Build.VERSION_CODES.JELLY_BEAN_MR2){
-                alarmManager.setExact(AlarmManager.RTC, tomorrow.getMillis() + 100, pending);
-            } else {
-                alarmManager.set(AlarmManager.RTC, tomorrow.getMillis() + 100, pending);
-            }
-
+            alarmManager.set(AlarmManager.RTC, tomorrow.getMillis() + 100, pending);
         }
     }
 
     /**
      * Update all widgets in the database
-     * @param context
-     * @param appWidgetManager
      */
-    private void updateAllWidgets(Context context, AppWidgetManager appWidgetManager){
+    private void updateAllWidgets(Context context, AppWidgetManager appWidgetManager) {
         List<WidgetInfo> infoList = DatabaseManager.getInstance(context).getAllWidgetInfo();
 
         // Log how many widgets there are
         FirebaseAnalytics.getInstance(context)
                 .setUserProperty(Analytics.PROPERTY_WIDGET_COUNT, ""+infoList.size());
 
-        for(WidgetInfo info : infoList){
+        for (WidgetInfo info : infoList) {
             int widgetId = info.getId();
 
             Log.d("Updating widget "+widgetId);
@@ -153,9 +143,9 @@ public class WidgetProvider extends AppWidgetProvider {
             RemoteViews remoteViews = updateWidgetView(context, info, mScreenShotMode);
 
             //Update it
-            try{
+            try {
                 appWidgetManager.updateAppWidget(widgetId, remoteViews);
-            } catch(Exception e){
+            } catch(Exception e) {
                 /*
                  * Catching all exceptions, because I suspect that if a widget has been deleted,
                  * yet not removed from the database, it will still try to update it and probably cause
@@ -171,22 +161,18 @@ public class WidgetProvider extends AppWidgetProvider {
 
     /**
      * Show the error view for the widget ID
-     * @param context
-     * @param appWidgetManager
-     * @param widgetId
      */
-    private void showErrorView(Context context, AppWidgetManager appWidgetManager, int widgetId){
+    private void showErrorView(Context context, AppWidgetManager appWidgetManager, int widgetId) {
         // Show the error message
         RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_error);
         try {
             appWidgetManager.updateAppWidget(widgetId, remoteViews);
-        } catch(Exception e){
+        } catch(Exception e) {
             // Ohwell. We tried
             Log.e("Uhoh showing error view", e);
         }
     }
-    
-    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+
     public void onAppWidgetOptionsChanged(Context context,
             AppWidgetManager appWidgetManager, int appWidgetId,
             Bundle newOptions) {
@@ -219,7 +205,7 @@ public class WidgetProvider extends AppWidgetProvider {
     @Override
     public void onDeleted(Context context, int[] appWidgetIds) {
         //Remove them from the database
-        for(int id: appWidgetIds){
+        for(int id: appWidgetIds) {
             DatabaseManager.getInstance(context).deleteWidgetInfo(id);
         }
         
@@ -229,7 +215,7 @@ public class WidgetProvider extends AppWidgetProvider {
     /**
      * Sets up and fills the RemoteViews with the data provided in the WidgetInfo class
      */
-    public static RemoteViews updateWidgetView(Context context, WidgetInfo info){
+    public static RemoteViews updateWidgetView(Context context, WidgetInfo info) {
         return updateWidgetView(context, info, false);
     }
 
@@ -237,7 +223,7 @@ public class WidgetProvider extends AppWidgetProvider {
      * Sets up and fills the RemoteViews with the data provided in the WidgetInfo class
      */
     public static RemoteViews updateWidgetView(Context context,
-                                               WidgetInfo info, boolean screenShotMode){
+                                               WidgetInfo info, boolean screenShotMode) {
         Deployment deployment = info.getDeployment();
         Resources resources = context.getResources();
         Prefs.load(context);
@@ -315,7 +301,7 @@ public class WidgetProvider extends AppWidgetProvider {
         Intent intent = new Intent(context, WidgetProvider.class);
 
         intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
-        int array[] = new int[1];
+        int[] array = new int[1];
         array[0] = info.getId();
         intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, array);
 
@@ -326,8 +312,8 @@ public class WidgetProvider extends AppWidgetProvider {
         
         return remoteViews;
     }
-    
-    public static Bitmap getChartBitmap(Deployment deployment, int size){
+
+    public static Bitmap getChartBitmap(@NonNull Deployment deployment, int size) {
     	//Set up the pie chart image
         Bitmap.Config conf = Bitmap.Config.ARGB_8888;
         Bitmap bmp = Bitmap.createBitmap(size,size,conf);
@@ -349,12 +335,7 @@ public class WidgetProvider extends AppWidgetProvider {
 		
 		float midX = size / 2f;
 		float midY = size / 2f;
-		float radius;
-		if (midX < midY){
-			radius = midX;
-		} else {
-			radius = midY;
-		}
+		float radius = Math.min(midX, midY);
 		radius -= PADDING;
 		float innerRadius = radius - thickness;
 		
