4dfe2b3962e82e37be121a859a7b380927a7f515
Scott Warner <Tortel1210@gmail.com>
2021-01-13 19:48:08 -0500

    Use view binding on deployment fragment

--- app/src/main/java/com/tortel/deploytrack/fragments/DeploymentFragment.java
+++ app/src/main/java/com/tortel/deploytrack/fragments/DeploymentFragment.java
@@ -16,8 +16,9 @@
 package com.tortel.deploytrack.fragments;
 
 import android.annotation.SuppressLint;
-import android.content.res.Resources;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import android.util.DisplayMetrics;
 import android.util.TypedValue;
@@ -26,13 +27,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.hookedonplay.decoviewlib.DecoView;
 import com.hookedonplay.decoviewlib.charts.SeriesItem;
 import com.hookedonplay.decoviewlib.events.DecoEvent;
 import com.tortel.deploytrack.Prefs;
 import com.tortel.deploytrack.R;
 import com.tortel.deploytrack.data.DatabaseManager;
 import com.tortel.deploytrack.data.Deployment;
+import com.tortel.deploytrack.databinding.FragmentDeploymentBinding;
 
 /**
  * Fragment that displays the fancy deployment graph and info
@@ -41,7 +42,7 @@ public class DeploymentFragment extends Fragment {
 	/**
 	 * Delay (In MS) before the circle starts its show animation
 	 */
-	private static final long ANI_SHOW_DELAY = 200;
+	private static final long ANI_SHOW_DELAY = 50;
 	/**
 	 * Length (In MS) of the show animation
 	 */
@@ -52,14 +53,13 @@ public class DeploymentFragment extends Fragment {
 	private static final long ANI_PERCENT_DURATION = 1000;
 	
 	private Deployment mDeployment;
+	private SeriesItem mCompletedSeries;
 	private TextView mPercentView;
 	private TextView mCompletedView;
-	private TextView mDateRangeView;
 	private TextView mRemainingView;
-	private Resources mResources;
-	private DecoView mArcView;
-	private SeriesItem mCompletedSeries;
 	private int mCompletedIndex;
+
+	private FragmentDeploymentBinding binding;
 
     private int mAnimatorType;
 
@@ -67,7 +67,7 @@ public class DeploymentFragment extends Fragment {
 	 * Creates a new DeploymentFragment with the provided
 	 * Deployment
 	 */
-	public static DeploymentFragment newInstance(Deployment deployment){
+	public static DeploymentFragment newInstance(Deployment deployment) {
 		DeploymentFragment fragment = new DeploymentFragment();
 		fragment.mDeployment = deployment;
 		fragment.setRetainInstance(false);
@@ -92,25 +92,23 @@ public class DeploymentFragment extends Fragment {
 	
 	@SuppressLint("SetTextI18n")
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState){
-		mResources = getActivity().getResources();
-
-		View view = inflater.inflate(R.layout.fragment_deployment, container, false);
+	public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+							 Bundle savedInstanceState){
+		binding = FragmentDeploymentBinding.inflate(inflater, container, false);
 
 		// Check for a valid deployment object
 		if (mDeployment == null) {
 			// Stop now
-			return view;
+			return binding.getRoot();
 		}
 
         // Set up the views
-        setUpTextViews(view);
+        setUpTextViews();
 
-		mDateRangeView.setText(mResources.getString(R.string.date_range,
+		binding.daterange.setText(getResources().getString(R.string.date_range,
                 mDeployment.getFormattedStart(), mDeployment.getFormattedEnd()));
-		mCompletedView.setText(mResources.getQuantityString(R.plurals.days_complete, mDeployment.getCompleted(), mDeployment.getCompleted()));
-        mRemainingView.setText(mResources.getQuantityString(R.plurals.days_remaining, mDeployment.getRemaining(), mDeployment.getRemaining()));
+		mCompletedView.setText(getResources().getQuantityString(R.plurals.days_complete, mDeployment.getCompleted(), mDeployment.getCompleted()));
+        mRemainingView.setText(getResources().getQuantityString(R.plurals.days_remaining, mDeployment.getRemaining(), mDeployment.getRemaining()));
 		
 		// Hide or display percent
 		if(Prefs.hidePercent()){
@@ -118,9 +116,6 @@ public class DeploymentFragment extends Fragment {
 		} else {
 		    mPercentView.setText(mDeployment.getPercentage() + "%");
 		}
-		
-		// Fill the graph
-		mArcView = (DecoView) view.findViewById(R.id.graph);
 
 		// Get the metrics
 		DisplayMetrics metrics = new DisplayMetrics();
@@ -133,7 +128,7 @@ public class DeploymentFragment extends Fragment {
 			.setLineWidth(size / 6f)
 			.setInitialVisibility(false);
 
-		mArcView.addSeries(backgroundBuilder.build());
+		binding.graph.addSeries(backgroundBuilder.build());
 
 		mCompletedSeries = new SeriesItem.Builder(mDeployment.getCompletedColor())
 				.setRange(0, mDeployment.getLength(), 0)
@@ -169,78 +164,80 @@ public class DeploymentFragment extends Fragment {
 			}
 		});
 
-		mCompletedIndex = mArcView.addSeries(mCompletedSeries);
+		mCompletedIndex = binding.graph.addSeries(mCompletedSeries);
 
-		return view;
+		return binding.getRoot();
 	}
-	
-	@SuppressLint("CutPasteId")
-	private void setUpTextViews(View view){
-		float density = getResources().getDisplayMetrics().density;
-		TextView commaView = (TextView) view.findViewById(R.id.comma);
-		mDateRangeView = (TextView) view.findViewById(R.id.daterange);
 
-        TextView main = (TextView) view.findViewById(R.id.main);
-        TextView second = (TextView) view.findViewById(R.id.second);
-        TextView third = (TextView) view.findViewById(R.id.third);
+	@Override
+	public void onDestroyView() {
+		super.onDestroyView();
+		binding = null;
+		mPercentView = null;
+		mCompletedView = null;
+		mRemainingView = null;
+	}
+
+	private void setUpTextViews(){
+		float density = getResources().getDisplayMetrics().density;
 		
 		switch(Prefs.getMainDisplayType()){
 		case Prefs.ViewTypes.PERCENT:
             mAnimatorType = Prefs.ViewTypes.PERCENT;
-			mPercentView = main;
-			mCompletedView = second;
-			mRemainingView = third;
+			mPercentView = binding.main;
+			mCompletedView = binding.second;
+			mRemainingView = binding.third;
 			break;
 		case Prefs.ViewTypes.COMPLETE:
             mAnimatorType = Prefs.ViewTypes.COMPLETE;
-			mCompletedView = main;
+			mCompletedView = binding.main;
 			mCompletedView.setTextSize(TypedValue.COMPLEX_UNIT_PX, mCompletedView.getTextSize() - density * 20f);
-			mPercentView = second;
-			mRemainingView = third;
+			mPercentView = binding.second;
+			mRemainingView = binding.third;
 			break;
 		case Prefs.ViewTypes.REMAINING:
             mAnimatorType = Prefs.ViewTypes.REMAINING;
-			mRemainingView = main;
+			mRemainingView = binding.main;
 			mRemainingView.setTextSize(TypedValue.COMPLEX_UNIT_PX, mRemainingView.getTextSize() - density * 10f);
-			mPercentView = second;
-			mCompletedView = third;
+			mPercentView = binding.second;
+			mCompletedView = binding.third;
 			break;
 		}
 
-        if(Prefs.hideDate()){
-            mDateRangeView.setVisibility(View.GONE);
+        if (Prefs.hideDate()) {
+            binding.daterange.setVisibility(View.GONE);
         }
 
         // Just hide days
-		if(Prefs.hideDays() && !Prefs.hidePercent()){
-            mPercentView = main;
+		if (Prefs.hideDays() && !Prefs.hidePercent()) {
+            mPercentView = binding.main;
             mAnimatorType = Prefs.ViewTypes.PERCENT;
 
             // Make sure nothing else is set to main
-            mCompletedView = second;
-            mRemainingView = second;
-            second.setVisibility(View.GONE);
-            third.setVisibility(View.GONE);
-            commaView.setVisibility(View.GONE);
+            mCompletedView = binding.second;
+            mRemainingView = binding.third;
+            mCompletedView.setVisibility(View.GONE);
+            mRemainingView.setVisibility(View.GONE);
+            binding.comma.setVisibility(View.GONE);
             return;
         }
 
         // Just hide percent
-        if(Prefs.hidePercent() && !Prefs.hideDays()){
+        if (Prefs.hidePercent() && !Prefs.hideDays()) {
             mPercentView.setVisibility(View.GONE);
-            if(Prefs.getMainDisplayType() != Prefs.ViewTypes.PERCENT){
+            if (Prefs.getMainDisplayType() != Prefs.ViewTypes.PERCENT) {
                 // Hide the comma too
-                commaView.setVisibility(View.GONE);
+                binding.comma.setVisibility(View.GONE);
             }
             return;
         }
 
         // If both are hidden, hide it all
-        if(Prefs.hideDays() && Prefs.hidePercent()){
-            commaView.setVisibility(View.GONE);
-            main.setVisibility(View.GONE);
-            second.setVisibility(View.GONE);
-            third.setVisibility(View.GONE);
+        if(Prefs.hideDays()) {
+            binding.comma.setVisibility(View.GONE);
+            binding.main.setVisibility(View.GONE);
+            binding.second.setVisibility(View.GONE);
+            binding.third.setVisibility(View.GONE);
         }
 	}
 	
@@ -253,24 +250,24 @@ public class DeploymentFragment extends Fragment {
 	@Override
 	public void onPause(){
 		super.onPause();
-		if(mArcView != null){
+		if (binding != null) {
 			// Clear all animations
-			mArcView.executeReset();
+			binding.graph.executeReset();
 		}
 	}
-	
-	private void animate(){
-		if(mArcView == null || mDeployment == null){
+
+	private void animate() {
+		if (binding == null || mDeployment == null) {
 			return;
 		}
 
-		if(!Prefs.isAnimationEnabled()){
-			mArcView.addEvent(new DecoEvent.Builder(DecoEvent.EventType.EVENT_SHOW, true)
+		if (!Prefs.isAnimationEnabled()) {
+			binding.graph.addEvent(new DecoEvent.Builder(DecoEvent.EventType.EVENT_SHOW, true)
 					.setDelay(0)
 					.setDuration(0)
 					.build());
 
-			mArcView.addEvent(new DecoEvent.Builder(mDeployment.getCompleted())
+			binding.graph.addEvent(new DecoEvent.Builder(mDeployment.getCompleted())
 					.setIndex(mCompletedIndex)
 					.setDelay(0)
 					.setDuration(0)
@@ -280,35 +277,35 @@ public class DeploymentFragment extends Fragment {
 			return;
 		}
 		// Remove all animation events
-		mArcView.executeReset();
+		binding.graph.executeReset();
 
-		mArcView.addEvent(new DecoEvent.Builder(DecoEvent.EventType.EVENT_SHOW, true)
+		binding.graph.addEvent(new DecoEvent.Builder(DecoEvent.EventType.EVENT_SHOW, true)
 				.setDelay(ANI_SHOW_DELAY)
 				.setDuration(ANI_SHOW_DURATION)
 				.build());
 
-		mArcView.addEvent(new DecoEvent.Builder(mDeployment.getCompleted())
+		binding.graph.addEvent(new DecoEvent.Builder(mDeployment.getCompleted())
 				.setIndex(mCompletedIndex)
 				.setDelay(ANI_SHOW_DELAY + ANI_SHOW_DURATION)
 				.setDuration(ANI_PERCENT_DURATION)
 				.build());
 	}
 	
-	private void setCompleted(int days){
-		if(mCompletedView != null && mResources != null){
-			mCompletedView.setText(mResources.getQuantityString(R.plurals.days_complete, days, days));
+	private void setCompleted(int days) {
+		if (mCompletedView != null) {
+			mCompletedView.setText(getResources().getQuantityString(R.plurals.days_complete, days, days));
 		}
 	}
 	
-	private void setRemaining(int days){
-		if(mRemainingView != null && mResources != null){
-			mRemainingView.setText(mResources.getQuantityString(R.plurals.days_remaining, days, days));
+	private void setRemaining(int days) {
+		if (mRemainingView != null) {
+			mRemainingView.setText(getResources().getQuantityString(R.plurals.days_remaining, days, days));
 		}
 	}
 	
 	@SuppressLint("SetTextI18n")
-	private void setPercent(int percent){
-		if(mPercentView != null){
+	private void setPercent(int percent) {
+		if (mPercentView != null) {
 			mPercentView.setText(percent+"%");
 		}
 	}
@@ -325,7 +322,7 @@ public class DeploymentFragment extends Fragment {
 	}
 
 	@Override
-	public void onSaveInstanceState(Bundle outState) {
+	public void onSaveInstanceState(@NonNull Bundle outState) {
 		super.onSaveInstanceState(outState);
 		if (mDeployment != null) {
 			outState.putString("id", mDeployment.getUuid());
