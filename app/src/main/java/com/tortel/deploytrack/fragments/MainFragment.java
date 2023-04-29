730f975a714a71b240695db7b8848bc28e3f50ac
Scott Warner <Tortel1210@gmail.com>
2023-04-28 21:26:56 -0400

    Update copyright
    Happy 10 years to this project!

--- app/src/main/java/com/tortel/deploytrack/fragments/MainFragment.java
+++ app/src/main/java/com/tortel/deploytrack/fragments/MainFragment.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 Scott Warner
+ * Copyright (C) 2023 Scott Warner
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
